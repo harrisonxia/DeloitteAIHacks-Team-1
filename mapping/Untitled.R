a7bdcefeb88ba4779e514aba846862ba282c6bec
harrisonx <engcxia@gmail.com>
2019-10-01 10:47:48 -0700

    leftover files.

---
+++ mapping/Untitled.R
@@ -0,0 +1,5 @@
+library(readxl)
+data <- read.csv("merged.csv") #Upload the data
+print(data)
+#lmHeight = lm(height~age, data = ageandheight) #Create the linear regression
+#summary(lmHeight) #Review the results
